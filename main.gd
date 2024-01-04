3fbe2fb43b999a42b0b9a40b8dea4a17a722a179
Carlos Tiago <ontiago@gmail.com>
2024-01-04 15:57:19 -0300

    create HUD

--- main.gd
+++ main.gd
@@ -6,7 +6,6 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#new_game()
 	pass
 
 
@@ -14,27 +13,32 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func _on_player_hit():
 	pass # Replace with function body.
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
 
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+	$HUD.update_score(score)
 
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout():
@@ -55,3 +59,7 @@ func _on_mob_timer_timeout():
 	mob.linear_velocity = velocity.rotated(direction)
 	
 	add_child(mob)
+
+
+func _on_hud_start_game():
+	pass # Replace with function body.
